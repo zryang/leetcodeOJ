b0caddee02fe6fa8678ada6f427f5aa921523813
Shaun Young <yzr2064@gmail.com>
2019-05-16 00:34:11 +0800

    add test code

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -1,4 +1,9 @@
-class Solution {
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution 
+{
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
@@ -13,3 +18,18 @@ public:
         }
     }
 };
+
+int main()
+{
+    Solution test;
+    vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(3);
+    nums.push_back(6);
+    nums.push_back(5);
+    int target = 8;
+    vector<int> ans = test.twoSum(nums, target);
+    for (int i = 0; i != ans.size(); i++)
+    cout<< ans[i] <<endl;
+    return 0;
+}
